8009f4231cbe131fe0b247c0842b0280dae28536
lucienchen <lucien.chen@qima.com>
2022-10-11 11:04:59 +0800

    optimized project update time

--- src/containers/news/NewsPage.tsx
+++ src/containers/news/NewsPage.tsx
@@ -1,23 +1,13 @@
 import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import {
-  Grid,
-  Card,
-  CardActionArea,
-  CardContent,
-  Typography,
-  CardHeader,
-  Avatar,
-  Divider,
-} from "@mui/material";
+import { Grid, Tooltip } from "@mui/material";
 import FlagIcon from "@mui/icons-material/Flag";
 import ArticleIcon from "@mui/icons-material/Article";
 import { Page } from "../../components";
-import { useNavigate } from "react-router-dom";
 import { server, ROOT_URL, SERVER_URI } from "../../servers";
 import theme from "../../theme";
-import { t } from "i18next";
 import moment from "moment";
+import { StandardNewsCard } from "./StandardNewsCard";
 
 interface NewsDTO {
   id: string;
@@ -40,19 +30,23 @@ export default function NewsPage() {
     switch (typeValue) {
       case "1":
         return (
-          <FlagIcon
-            sx={{
-              fontSize: "1.3rem",
-              mr: 1,
-              color: theme.palette.warning.main,
-            }}
-          />
+          <Tooltip title={t("news-page.news-types.project")}>
+            <FlagIcon
+              sx={{
+                fontSize: "1.3rem",
+                mr: 1,
+                color: theme.palette.warning.main,
+              }}
+            />
+          </Tooltip>
         );
       case "2":
         return (
-          <ArticleIcon
-            sx={{ fontSize: "1.3rem", mr: 1, color: theme.palette.info.main }}
-          />
+          <Tooltip title={t("news-page.news-types.article")}>
+            <ArticleIcon
+              sx={{ fontSize: "1.3rem", mr: 1, color: theme.palette.info.main }}
+            />
+          </Tooltip>
         );
       default:
         return null;
@@ -103,7 +97,7 @@ export default function NewsPage() {
               creater: n.ownerName,
               avatarUrl: n.avatarUrl,
               createTime: format(n.createTime),
-              updateTime: format(n.updateTime || ""),
+              updateTime: n.updateTime ? format(n.updateTime) : null,
               publishTime: n.publishTime
                 ? format(n.publishTime)
                 : format(n.createTime),
@@ -116,63 +110,3 @@ export default function NewsPage() {
     </Page>
   );
 }
-interface StandardNewsCardProps {
-  data: News;
-}
-
-interface News {
-  title: string;
-  description: string;
-  newsTypeIcon: any;
-  creater: string;
-  avatarUrl: string;
-  createTime: string;
-  updateTime: string;
-  publishTime: string;
-  detailsUrl: string;
-}
-
-const StandardNewsCard = (props: StandardNewsCardProps) => {
-  const navigate = useNavigate();
-  return (
-    <Grid item xs={12} sm={6} md={4} lg={3}>
-      <Card variant="outlined" sx={{ pb: 0 }}>
-        <CardHeader
-          avatar={
-            <Avatar alt={props.data.creater} src={props.data.avatarUrl} />
-          }
-          title={props.data.creater}
-          subheader={t("news-page.publish-at") + props.data?.publishTime}
-        />
-        <Divider />
-        <CardActionArea onClick={() => navigate(props.data.detailsUrl)}>
-          <CardContent>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ justifyContent: "center" }}
-            >
-              {props.data.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {props.data.description}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <Divider />
-        <CardContent sx={{ my: 0, py: 0 }}>
-          <Grid container sx={{ pt: 1 }}>
-            <Grid item sx={{ flexGrow: 0 }}>
-              {props.data.newsTypeIcon}
-            </Grid>
-            <Grid item sx={{ flexGrow: 1 }}>
-              <Typography variant="body2" color="text.secondary">
-                {t("news-page.update-at") + props.data?.updateTime}
-              </Typography>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-    </Grid>
-  );
-};
