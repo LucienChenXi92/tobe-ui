ea7b78f5f5f7656cb4afb4a73944fc1804157ad1
lucienchen <lucien.chen@qima.com>
2023-01-18 17:45:05 +0800

    Fixed warnings

--- src/containers/frontend/homePage/HomePage.tsx
+++ src/containers/frontend/homePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import { BasicLayout } from "../../../components";
 import GreatingSection from "./GreatingSection";
-import NavigationSection from "./NavigationSection";
+// import NavigationSection from "./NavigationSection";
 import FunctionSection from "./FunctionSection";
 
 export default function HomePage() {
