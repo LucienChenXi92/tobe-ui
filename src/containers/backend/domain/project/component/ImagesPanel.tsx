fa2801a8a6794e1b03c30396625422ee4365195e
lucienchen <lucien.chen@qima.com>
2024-04-05 16:00:28 +0800

    Compress files

--- src/containers/backend/domain/project/component/ImagesPanel.tsx
+++ src/containers/backend/domain/project/component/ImagesPanel.tsx
@@ -3,32 +3,23 @@ import { Grid, Link } from "@mui/material";
 export const ImagesPanel = (props: { imageURLs: string[] }) => (
   <Grid item container sx={{ mt: 1 }}>
     {props.imageURLs.length > 0 && (
-      <Grid container>
+      <Grid container sx={{ width: "100%" }}>
         {props.imageURLs.map((imgURL: string) => (
-          <Grid
-            container
-            item
-            xs={6}
-            sm={4}
-            md={3}
-            lg={2}
-            xl={2}
-            sx={{ p: 1, border: "0.5px solid rgba(0,0,0,0.12)" }}
-            key={imgURL}
-          >
-            <Link target="_blank" href={imgURL}>
-              <img
-                src={imgURL}
-                alt={imgURL}
-                loading="lazy"
-                decoding="async"
-                style={{
-                  width: "100%",
-                  height: "150px",
-                }}
-              />
-            </Link>
-          </Grid>
+          <Link target="_blank" href={imgURL}>
+            <Grid
+              container
+              sx={{
+                border: "0.5px solid rgba(0,0,0,0.12)",
+                backgroundImage: `url(${imgURL})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center 40%",
+                height: "200px",
+                width: "275px",
+                p: 1,
+              }}
+              key={imgURL}
+            />
+          </Link>
         ))}
       </Grid>
     )}
--- src/services/FileService.ts
+++ src/services/FileService.ts
@@ -1,4 +1,5 @@
 import { AxiosPromise } from "axios";
+import Compressor from "compressorjs";
 import { server } from ".";
 
 const FILE_URI = "v1/files";
@@ -24,21 +25,50 @@ export function upload(
 export function batchUpload(
   srcId: string,
   fileType: string,
-  files: Blob[]
+  files: File[]
 ): AxiosPromise {
   const formData = new FormData();
-  files.forEach((f) => {
-    formData.append("files", f);
+  return compressImages(files).then((compressedFiles: File[]) => {
+    compressedFiles.forEach((f: File) => {
+      formData.append("files", f, f.name);
+    });
+    return server.post(
+      `/${FILE_URI}/batch-upload?srcId=${srcId}&fileType=${fileType}`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
   });
-  return server.post(
-    `/${FILE_URI}/batch-upload?srcId=${srcId}&fileType=${fileType}`,
-    formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
+}
+
+function compressImages(files: File[]): Promise<File[]> {
+  return new Promise(async function (resolve) {
+    let processedImages = 0;
+    let numImagesToProcess = files.length;
+    for (let i = 0; i < numImagesToProcess; i++) {
+      const file = files[i];
+      await new Promise((resolve) => {
+        new Compressor(file, {
+          quality: 0.5,
+          success(result: File) {
+            files[i] = result;
+            resolve("Done");
+          },
+          error: () => {
+            resolve("Failed");
+          },
+        });
+      });
+      processedImages += 1;
     }
-  );
+    debugger;
+    if (processedImages === numImagesToProcess) {
+      resolve(files);
+    }
+  });
 }
 
 export function getBySrcIdAndFileType(srcId: string, fileType: string) {
